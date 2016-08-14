b828441566cfb1dff6c5ceb6cc7a896c61a96daa
Andrelton <eric.saxman@gmail.com>
2016-08-14 02:34:54 -0700

    temporarily disable match time functionality until issue is resolved

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,31 +1,30 @@
 $(document).ready(function() {
-  var fixtureSpans = $('span.next-fixture-date');
-  fixtureSpans.each(function() {
-    var element = $(this);
+  // var fixtureSpans = $('span.next-fixture-date');
+  // fixtureSpans.each(function() {
+  //   var element = $(this);
 
-    var dateObject = new Date(element.text());
+  //   var dateObject = new Date(element.text());
 
-    var hours = dateObject.getHours();
+  //   var hours = dateObject.getHours();
+  //   var minutes = dateObject.getMinutes();
+  //   if (minutes === 0) {
+  //     minutes = "00"
+  //   }
 
-    var minutes = dateObject.getMinutes();
-    if (minutes === 0) {
-      minutes = "00"
-    }
+  //   // set AM or PM...
+  //   var period = hours < 12 ? "a" : "p"
+  //   // ...then change to 12-hour format
+  //   if (hours > 12) {
+  //     hours = hours -12
+  //   }
 
-    // set AM or PM...
-    var period = hours < 12 ? "a" : "p"
-    // ...then change to 12-hour format
-    if (hours > 12) {
-      hours = hours -12
-    }
+  //   element.text("(" + hours + ":" + minutes + period + ")")
 
-    element.text("(" + hours + ":" + minutes + period + ")")
+  //   // moment.js ...which isn't working on mobile/Safari
+  //   // var momentObject = moment(new Date(element.text()));
+  //   // var newText = momentObject.format('h:mma').slice(0, -1);
+  //   // element.text("(" + newText + ")");
 
-    // moment.js ...which isn't working on mobile/Safari
-    // var momentObject = moment(new Date(element.text()));
-    // var newText = momentObject.format('h:mma').slice(0, -1);
-    // element.text("(" + newText + ")");
-
-    element.show();
-  })
+  //   element.show();
+  // })
 });
