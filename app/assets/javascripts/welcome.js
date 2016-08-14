47e2b6b39ef3f79027dc9d59abc0c4a5d2d2415c
Andrelton <eric.saxman@gmail.com>
2016-08-14 00:42:14 -0700

    add initial match time functionality, locally-appropriate

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,23 @@
-// $(document).ready(function() {
-//   var fixtureSpans = $('span.next-fixture-date');
-//   console.log(fixtureSpans[0]);
-// });
+$(document).ready(function() {
+  var fixtureSpans = $('span.next-fixture-date');
+  fixtureSpans.each(function() {
+    var element = $(this);
+
+    // var dateObject = new Date(element.text());
+
+    // var hours = dateObject.getHours();
+    // var minutes = dateObject.getMinutes();
+    // // AM or PM
+    // var period = hours < 12 ? "a" : "p"
+
+    // element.text("(" + hours + ":" + minutes + period + ")")
+
+    console.log(element.text());
+
+    var momentObject = moment(new Date(element.text()));
+    var newText = momentObject.format('h:mma').slice(0, -1);
+    element.text("(" + newText + ")");
+
+    element.show();
+  })
+});
