d1b91aac3def8ccdc6a5defd6706460820ae8a78
Andrelton <eric.saxman@gmail.com>
2016-08-14 09:56:51 -0700

    add time functionality

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,30 +1,35 @@
 $(document).ready(function() {
-  // var fixtureSpans = $('span.next-fixture-date');
-  // fixtureSpans.each(function() {
-  //   var element = $(this);
+  var fixtureSpans = $('span.next-fixture-date');
+  fixtureSpans.each(function() {
+    var element = $(this);
 
-  //   var dateObject = new Date(element.text());
+    var dateObject = new Date(parseInt(element.text()));
+    console.log(element.text());
 
-  //   var hours = dateObject.getHours();
-  //   var minutes = dateObject.getMinutes();
-  //   if (minutes === 0) {
-  //     minutes = "00"
-  //   }
+    var hours = dateObject.getHours();
+    var minutes = dateObject.getMinutes();
+    if (minutes === 0) {
+      minutes = "00"
+    }
 
-  //   // set AM or PM...
-  //   var period = hours < 12 ? "a" : "p"
-  //   // ...then change to 12-hour format
-  //   if (hours > 12) {
-  //     hours = hours -12
-  //   }
+    // set AM or PM...
+    var period = hours < 12 ? "a" : "p"
+    // ...then change to 12-hour format
+    if (hours > 12) {
+      hours = hours -12
+    }
 
-  //   element.text("(" + hours + ":" + minutes + period + ")")
+    console.log(dateObject);
 
-  //   // moment.js ...which isn't working on mobile/Safari
-  //   // var momentObject = moment(new Date(element.text()));
-  //   // var newText = momentObject.format('h:mma').slice(0, -1);
-  //   // element.text("(" + newText + ")");
+    element.text("(" + hours + ":" + minutes + period + ")")
 
-  //   element.show();
-  // })
+    // console.log(element.text());
+
+    // moment.js ...which isn't working on mobile/Safari
+    // var momentObject = moment(new Date(element.text()));
+    // var newText = momentObject.format('h:mma').slice(0, -1);
+    // element.text("(" + newText + ")");
+
+    element.show();
+  })
 });
